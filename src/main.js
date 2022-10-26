d20002a9e83fe50ac898ecae5dd4b7b65e468cb3
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-10-26 11:55:52 +0200

    gsap 100%

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from "./router";
 import store from "./store";
 /* import AOS from "aos"; */
 import "aos/dist/aos.css";
+import { gsap } from "gsap";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(gsap).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,152 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
+	<h1 class="text-3xl font-bold underline text-cyan-300">H!</h1>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card bg-gray-600"
+		style="width: 18rem; margin: 200px auto"
+	>
+		azeaz
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	name: "HomeView",
+	components: {
+		HelloWorld,
+	},
+};
 </script>
