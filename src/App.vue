35e63a264a692062a6269e1f8596beedeeb1eecf
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-10-26 10:29:45 +0200

    settingup aos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,173 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<nav>
+		<router-link to="/">Home</router-link> |
+		<router-link to="/about">About</router-link>
+	</nav>
+	<router-view />
+	<h1 class="text-3xl font-bold underline text-cyan-300">H!</h1>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card bg-gray-600"
+		style="width: 18rem; margin: 200px auto"
+	>
+		azeaz
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	></div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
+	<div
+		data-aos="flip-left"
+		class="card"
+		style="width: 18rem; margin: 200px auto"
+	>
+		lorem shiot
+	</div>
 </template>
-
-<style>
+<script>
+import AOS from "aos";
+export default {
+	mounted() {
+		AOS.init();
+	},
+};
+</script>
+<style src="./style.css"></style>
+<!-- <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
+ -->
