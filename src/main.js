319b6ae9b245c54e7c23cfe33c1b128e73c984e7
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-10-31 18:54:42 +0100

    adding fontawesome

--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@ import store from "./store";
 /* import AOS from "aos"; */
 import "aos/dist/aos.css";
 import { gsap } from "gsap";
+/* fontawesome */
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 createApp(App).use(store).use(gsap).use(router).mount("#app");
