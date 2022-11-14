42a913696212b8b956f6f356abd2d9003c44da3a
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-11-14 18:46:48 +0100

    --

---
+++ src/views/TryView.vue
@@ -0,0 +1,74 @@
+<template>
+	<MenuBar></MenuBar>
+
+	<div
+		class="bg-black h-[calc(100vh_-_64px)] lg:h-[calc(100vh_-_96px)] w-full mt-16 lg:mt-24 overflow-hidden"
+	>
+		<swiper
+			:navigation="true"
+			:modules="modules"
+			class="mySwiper text-center h-full text-white"
+		>
+			<swiper-slide
+				class="flex justify-center items-center bg-red-300 h-full w-full"
+				@click="hi"
+			>
+				<div
+					id="container "
+					class="h-full w-full lg:w-10/12 bg-gray-800 flex border-2 border-amber-600 overflow-hidden cursor-pointer"
+				>
+					<div
+						id="imgProject"
+						class="w-10/12 lg:w-11/12 bg-black-700 min-h-full overflow-hidden"
+					>
+						<img
+							src="https://new.dev-web.paris/api/projects/14-small.jpg"
+							class="min-w-full object-contain lg:object-fill"
+							alt=""
+						/>
+					</div>
+					<div
+						id="title"
+						class="w-2/12 lg:w-1/12 h-full bg-black flex flex-col items-center justify-between p-9 font-Poppins"
+					>
+						<div class="flex flex-col justify-center items-center text-xl">
+							<div class="mb-1">1</div>
+							<div class="h-0.5 w-5 bg-amber-500"></div>
+						</div>
+						<div class="w-96 -rotate-90">
+							<div class="text-right">
+								Gestion Cabinet Meddahi
+								<span class="text-amber-500 text-xl">.</span>
+							</div>
+							<div class="text-amber-500 text-xl">2021</div>
+						</div>
+					</div>
+				</div>
+			</swiper-slide>
+		</swiper>
+	</div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper";
+import MenuBar from "@/components/MenuBar.vue";
+export default {
+	components: { MenuBar, Swiper, SwiperSlide },
+	methods: {
+		hi() {},
+	},
+	data() {
+		return {
+			modules: [Navigation],
+		};
+	},
+};
+</script>
+
+<style scoped></style>
